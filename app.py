488975a4f86e4d5f68556cc6eaa91124d05d8eea
Jenny <12652728+jeunlee@users.noreply.github.com>
2025-04-03 21:31:50 -0400

    sketch out steps and add passcode

--- app.py
+++ app.py
@@ -23,6 +23,18 @@ def view3():
 def step1():
     return render_template('step1.html')
 
+@app.route('/step2')
+def step2():
+    return render_template('step2.html')   
+
+@app.route('/step3')
+def step3():
+    return render_template('step3.html')
+
+@app.route('/step4')
+def step4():
+    return render_template('step4.html')
+
 @app.route('/')
 def landing():
     return render_template('landing.html')
