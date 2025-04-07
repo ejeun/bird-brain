f34f7c97ec0e67c1e206c69a3f15d97a71c2fb4f
Jenny <12652728+jeunlee@users.noreply.github.com>
2025-04-07 16:31:36 -0400

    tune the timing for the intake process and styles

--- static/js/intake.js
+++ static/js/intake.js
@@ -9,8 +9,50 @@ import {
 
 const collage = document.getElementById('collage');
 const nameForm = document.getElementById('nameForm');
+const canvas = document.getElementById('visualization');
 let selectedCount = 0;
+
+const onLoad = async () => {
+    const intro = "First we will locate the life processes that match your signature.";
+    const follow = "Do not analyze. Locate.";
+    window.captions.show(intro);
+    window.captions.show(follow, false);
     
+    if (!canvas) {
+        console.error("Canvas element with id 'visualization' not found");
+    }
+    const dataviz = new DataVisualizer(canvas, CANVAS_CONFIG);
+    dataviz.setupNodes();
+    dataviz.animate();
+
+    function fadeOutCanvas(resolve) {
+        setTimeout(() => {
+            window.captions.show(LINES[0]);
+            canvas.style.opacity = '0';
+            canvas.style.transition = 'opacity 1s ease';
+            setTimeout(() => {
+                canvas.remove();
+                resolve();
+            }, ANIMATION_TIMING.CANVAS_FADE_OUT_DURATION);
+        }, ANIMATION_TIMING.CANVAS_FADE_OUT_DELAY);
+    }
+
+    // Fade out the canvas after the delay.
+    await new Promise(resolve => fadeOutCanvas(resolve));
+
+    if (!collage) {
+        console.error("Collage element with id 'collage' not found");
+    }
+    // Fade in the collage after the delay.
+    setTimeout(() => {
+        collage.style.opacity = '1';
+    }, ANIMATION_TIMING.COLLAGE_FADE_IN_DELAY);
+    setTimeout(() => {
+        collage.classList.remove('inactive');
+    }, ANIMATION_TIMING.COLLAGE_FADE_IN_DELAY + 3000);
+};
+
+document.addEventListener("DOMContentLoaded", onLoad);
 // Create and position images
 COLLAGE_IMAGES.forEach((img, index) => {
     const div = document.createElement('div');
@@ -27,108 +69,51 @@ COLLAGE_IMAGES.forEach((img, index) => {
     div.appendChild(image);
     collage.appendChild(div);
 
-    div.addEventListener('click', () => {
+    const onImageClick = () => {
         if (!div.classList.contains('selected') && selectedCount < 3) {
-            // window.captions.show(POETRY[index], false);
             div.classList.add('selected');
             selectedCount++;
         }
         if (selectedCount === 3) {
             // Add a class to the collage container to disable hover effects
-            collage.classList.add('selection-complete');
+            collage.classList.add('inactive');
+            // Fade out the collage.
+            setTimeout(() => {
+                collage.style.opacity = '0.35';
+                collage.style.transition = 'opacity 1s ease';
+            }, ANIMATION_TIMING.COLLAGE_FADE_OUT_DELAY);
+
             window.captions.show('Understood.');
             window.captions.show(LINES[1]);
-            // Wait for the poetry to finish before showing the name form.
             setTimeout(() => {
                 nameForm.classList.add('visible');
-            }, 3000);
+            }, ANIMATION_TIMING.FORM_DELAY);
         }
-    });
+    }
+    div.addEventListener('click', onImageClick);
 });
-
-const onDOMContentLoaded = () => {
-    const canvas = document.getElementById('visualization');
-    if (canvas) {
-        // Set canvas style to center it
-        canvas.style.position = 'absolute';
-        canvas.style.top = '50%';
-        canvas.style.left = '50%';
-        canvas.style.transform = 'translate(-50%, -50%)';
-        
-        const dataviz = new DataVisualizer(canvas, CANVAS_CONFIG);
-        dataviz.setupNodes();
-        dataviz.animate();
-        
-        // Set up a MutationObserver to watch for changes to the canvas display style
-        const observer = new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => {
-                if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-                    const displayStyle = window.getComputedStyle(canvas).display;
-                    if (displayStyle === 'none') {
-                        // This function will be called when the canvas display becomes 'none'
-                        onCanvasHidden();
-                    }
-                }
-            });
-        });
-        
-        // Start observing the canvas element for style attribute changes
-        observer.observe(canvas, { attributes: true });
-        
-    } else {
-        console.error("Canvas element with id 'visualization' not found");
-    }
-
-    // Fade out the canvas
-    setTimeout(() => {
-        window.captions.show(LINES[0]);
-        canvas.style.opacity = '0';
-        canvas.style.transition = 'opacity 1s ease';
-        setTimeout(() => {
-            canvas.style.display = 'none';
-        }, ANIMATION_TIMING.CANVAS_FADE_OUT_DURATION);
-    }, ANIMATION_TIMING.CANVAS_FADE_OUT_DELAY);   
-};
-
-// Function that will be called when the canvas is hidden
-const onCanvasHidden = () => {
-    const collage = document.getElementById('collage');
-    if (collage) {
-        collage.style.display = 'block';
-        collage.style.opacity = '0';
-        collage.style.transition = 'opacity 1s ease';
-        
-        // Fade in the collage
-        setTimeout(() => {
-            collage.style.opacity = '1';
-        }, ANIMATION_TIMING.COLLAGE_FADE_IN_DELAY);
-    }
-}
 
 // Handle name submission
 nameForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.getElementById('nameInput').value;
     const collage = document.getElementById('collage');
-    nameForm.classList.remove('visible');
-    collage.style.opacity = '0';
-    // Fade out the collage.
+
     setTimeout(() => {
-        collage.style.display = 'none';
-    }, ANIMATION_TIMING.COLLAGE_FADE_IN_DELAY);
+        nameForm.classList.remove('visible');
+    }, 100);
+
+    setTimeout(() => {
+        collage.style.opacity = '0';
+        // Fade out the collage.
+        setTimeout(() => {
+            collage.style.display = 'none';
+        }, ANIMATION_TIMING.COLLAGE_FADE_IN_DELAY);
+    }, 100);
     
     // Continue with the rest of the captions
     for (let i = 2; i < LINES.length; i++) {
         let text = LINES[i].replace('[name]', name);
         window.captions.show(text);
     }
-});
-
-document.addEventListener("DOMContentLoaded", () => { 
-    const intro = "First we will locate the life processes that match your signature.";
-    const follow = "Do not analyze. Locate.";
-    window.captions.show(intro);
-    window.captions.show(follow, false);
-    
-    onDOMContentLoaded();
 });
